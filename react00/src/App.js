92a43c43b1ea1b07d8e12cbe3d9a2bf56790ecac
Hyeonkkyu <101560173+Hyeonkkyu@users.noreply.github.com>
2022-07-25 12:22:01 +0900

    22.07.25

---
+++ react00/src/App.js
@@ -0,0 +1,18 @@
+import Test from "./test";
+import Nav from "./Nav";
+
+const App = () => {
+  //console.log('하하하')
+  return (
+    <>
+      <Test />
+      <h1>하하하</h1>
+      <Test />
+      <Test />
+      {/* <h2>히히히</h2> */}
+    </>
+  )
+
+}
+
+export default App;
---
+++ react00/src/test.js
@@ -0,0 +1,12 @@
+function Test() {
+    return (
+        <>
+        <i className="xi-bars"></i>
+         <p>
+            왜 나는 안 되는 걸까...
+         </p>
+        </>
+    )
+}
+
+export default Test;
